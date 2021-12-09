b12812e3bb37528c2cc762eeecb278082cbe953c
Roald Ong <roald.ong@gmail.com>
2021-12-09 12:47:16 +0800

    Added in table example

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
-import React, {
-  Component
-} from 'react'
+import React, {Component} from "react";
+
+const TableHeader = () => {
+	return (
+		<thead>
+			<tr>
+				<th>Name</th>
+				<th>Job</th>
+			</tr>
+		</thead>
+	);
+};
+
+const TableBody = () => {
+	return (
+		<tbody>
+			<tr>
+				<td>Charlie</td>
+				<td>Janitor</td>
+			</tr>
+			<tr>
+				<td>Mac</td>
+				<td>Bouncer</td>
+			</tr>
+			<tr>
+				<td>Dee</td>
+				<td>Aspiring actress</td>
+			</tr>
+			<tr>
+				<td>Dennis</td>
+				<td>Bartender</td>
+			</tr>
+		</tbody>
+	);
+};
 
 class App extends Component {
-  render() {
-    return ( <
-      div className = "App" >
-      <
-      h1 > Hello, reactz! < /h1> < /
-      div >
-    )
-  }
+	render() {
+		return (
+			<table>
+				<TableHeader />
+				<TableBody />
+			</table>
+		);
+	}
 }
 
-export default App
+export default App;
