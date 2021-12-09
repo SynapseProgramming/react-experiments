56cf2b8f8c1bf45773d96464c369888a81474e9f
Roald Ong <roald.ong@gmail.com>
2021-12-09 12:57:46 +0800

    Modified table files to be separate from App.js

--- src/App.js
+++ src/App.js
@@ -1,46 +1,12 @@
 import React, {Component} from "react";
-
-const TableHeader = () => {
-	return (
-		<thead>
-			<tr>
-				<th>Name</th>
-				<th>Job</th>
-			</tr>
-		</thead>
-	);
-};
-
-const TableBody = () => {
-	return (
-		<tbody>
-			<tr>
-				<td>Charlie</td>
-				<td>Janitor</td>
-			</tr>
-			<tr>
-				<td>Mac</td>
-				<td>Bouncer</td>
-			</tr>
-			<tr>
-				<td>Dee</td>
-				<td>Aspiring actress</td>
-			</tr>
-			<tr>
-				<td>Dennis</td>
-				<td>Bartender</td>
-			</tr>
-		</tbody>
-	);
-};
+import Table from "./Table";
 
 class App extends Component {
 	render() {
 		return (
-			<table>
-				<TableHeader />
-				<TableBody />
-			</table>
+			<div className="contain">
+				<Table />
+			</div>
 		);
 	}
 }
